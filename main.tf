e15eacce3fa609ed5a2183ac4a4c1c26a9bc9b55
venkatapavan99 <84392859+venkatapavan99@users.noreply.github.com>
2025-02-16 22:18:24 +0530

    Update main.tf
    Commited

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 variable "instance_type" {
+  description = " Type"
   type = string
 }
 variable "region" {
+  description = " Type"
   type = string
 }
 variable "ami_id" {
+  description = " Type"
   type = string
 }
 
 provider "aws" {
+  region = var.region
   }
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
